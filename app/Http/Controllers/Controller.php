5233136d0cd03219064eab70d352983cc0d028b5
rahul26021999 <rahul26021999@gmail.com>
2020-03-26 17:40:22 +0530

    swagger dependency added run composer update after this

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,46 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+// use OpenApi;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="L5 OpenApi",
+     *      description="L5 Swagger OpenApi description",
+     *      @OA\Contact(
+     *          email="customer@prepareurself"
+     *      ),
+     *     @OA\License(
+     *         name="Apache 2.0",
+     *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *     )
+     * )
+     */
 }
+    /**
+     * @OA\Get(
+     *     path="/store",
+     *     tags={"store"},
+     *     summary="Returns pet inventories by status",
+     *     description="Returns a map of status codes to quantities",
+     *     operationId="getInventory",
+     *     @OA\Response(
+     *         response=200,
+     *          description="successful operation",
+     *          @OA\JsonContent(
+     *              @OA\AdditionalProperties(
+     *                  type="integer",
+     *                  format="int32"
+     *              )
+     *          )
+     *     ),
+     *     security={
+     *         {"api_key": {}}
+     *     }
+     * )
+     */
