1708bcd758c011b2e7bb05a00930c0583d1a8a19
rahul26021999 <rahul26021999@gmail.com>
2020-04-08 14:16:35 +0530

    JWt added to composer json

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin.auth' => \App\Http\Middleware\AdminAuthMiddleware::class,
+        'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
+        'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
     ];
 
     /**
