ec325aca4bb821a1d5294fbfb57b5c768f54c371
rahul26021999 <rahul26021999@gmail.com>
2020-04-17 23:17:04 +0530

    jwt Status code changed in case of failures

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -21,11 +21,11 @@ class JwtMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['success'=>false,'message' => 'Token is Invalid']);
+                return response()->json(['success'=>false,'message' => 'Token is Invalid'],401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['success'=>false,'message' => 'Token is Expired']);
+                return response()->json(['success'=>false,'message' => 'Token is Expired'],402);
             }else{
-                return response()->json(['success'=>false,'message' => 'Authorization Token not found']);
+                return response()->json(['success'=>false,'message' => 'Authorization Token not found'],400);
             }
         }
         return $next($request);
