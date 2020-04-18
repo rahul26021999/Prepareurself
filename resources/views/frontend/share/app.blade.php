35dd56d6757acf95a2e41ef893a78e4d0fa8f169
rahul26021999 <rahul26021999@gmail.com>
2020-04-18 16:40:55 +0530

    share home screem done

--- resources/views/frontend/share/app.blade.php
+++ resources/views/frontend/share/app.blade.php
@@ -15,7 +15,7 @@
     <script>
     window.onload = function() {
     // <!-- Deep link URL for existing users with app already installed on their device -->
-      window.location ="{{$link}}";
+      window.location ="prepareurself_home://prepareurself.tk/?screen=.Home.ui.HomeActivity";
     
     // <!-- Download URL (TUNE link) for new users to download the app -->
         setTimeout("window.location = 'https://play.google.com/store/apps/details?id=com.playit.videoplayer';", 500);
