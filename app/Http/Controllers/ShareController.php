602502c8781c031d230a5ad4241afe155f390cf0
rahul26021999 <rahul26021999@gmail.com>
2020-04-21 21:57:46 +0530

    logs added in share

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Models\Resource;
 use App\Models\Course;
+use Log;
 
 class ShareController extends Controller
 {
@@ -17,19 +18,27 @@ class ShareController extends Controller
     {
     	$resource_id=base64_decode($id);
     	$resource=Resource::find($resource_id);
-    	if($resource!=null)
+    	if($resource!=null){
+            Log::alert('resource share called');
     		return view('frontend.share.app',['id'=>$resource_id,'type'=>$resource->type]);
-    	else
-    	   return view('frontend.share.app');
+        }
+    	else{
+            Log::alert('resource share not found');
+    	   return view('frontend.share.app',['type'=>'home']);
+        }
     }
     public function shareProject($id)
     {
     	$project_id=base64_decode($id);
     	$project=Project::find($project_id);
-    	if($project!=null)
+    	if($project!=null){
+            Log::alert('project share ');
     		return view('frontend.share.app',['id'=>$project_id,'type'=>'project']);
-    	else
+        }
+    	else{
+            Log::alert('project  share not found');
     	   return view('frontend.share.app',['type'=>'home']);
+        }
     	
     }
     public function shareCourse($id)
@@ -37,9 +46,13 @@ class ShareController extends Controller
 
     	$course_id=base64_decode($id);
     	$course=Course::find($course_id);
-    	if($course!=null)
+    	if($course!=null){
+            Log::alert('course share ');
             return view('frontend.share.app',['id'=>$course_id,'type'=>'course']);
-        else
+        }
+        else{
+            Log::alert('cousre share not found');
            return view('frontend.share.app',['type'=>'home']);
+        }
     }
 }
