f3de688ff45ee38bc51e56d3a17fba6390c9a381
tanyagarg2509 <tanyagarg2509@gmail.com>
2020-04-29 23:42:01 +0530

    Mail wala design krdiya pr alg se component ni hue

--- resources/views/emails/user/UserFeedback.blade.php
+++ resources/views/emails/user/UserFeedback.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
 # Hey, {{ $user['first_name'] }}
 
-Thankyou, for giving us your valuable feedback.
+<b class="appColor">Thankyou</b>, for giving us your valuable feedback.
 <br>
 We are on our way to provide you with best Resources.
 <br>
 Keep Exploring and Keep Learning!!
 
 Thanks<br>
-{{ config('app.name') }}
+<b class="appColor">{{ config('app.name') }}</b>
 @endcomponent
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,7 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}">
+<div class="logo"><img src="{{asset('logo.svg')}}" width="70px" height="50px"> </div>
+<a class="heading" href="{{ $url }}">
 {{ $slot }}
 </a>
 </td>
