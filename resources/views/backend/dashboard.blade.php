02ac6679f2f94eadf3e2bbd90414f969aac01a6c
rahul26021999 <rahul26021999@gmail.com>
2020-05-02 21:40:22 +0530

    sdads

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -123,7 +123,7 @@
           <input type="text" id="token" class="form-control" value="{{ $JWTtoken }}">
           <button class="btn btn-warning" id="tokenCopy">Default JWT Token</button>
           <a class="btn btn-primary mb-3 mt-3 ml-2" href="/7Sd6c5pcpWvzqFf4tF7S9e2HDxGVgp/phpmyadmin/">Phpmyadmin</a>
-          <a href="/api/documentation" target="_blank" class="btn btn-success mb-3 mt-3 ml-2">API Documentation</a>
+          <a href="/admin/api/documentation" target="_blank" class="btn btn-success mb-3 mt-3 ml-2">API Documentation</a>
         </div>
       </div>
       <!-- /.row -->
