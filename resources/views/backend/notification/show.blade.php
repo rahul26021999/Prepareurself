97d684dc5046c4c70c55706c937e60f3d857526b
rahul26021999 <rahul26021999@gmail.com>
2020-05-03 07:59:18 +0530

    reindentation completed

--- resources/views/backend/notification/show.blade.php
+++ resources/views/backend/notification/show.blade.php
@@ -26,61 +26,61 @@
 
 <script type="text/javascript">
 
-    $( "#screen" ).on('change',function() {
-        if($(this).val()=="app" || $(this).val()=="feedback")
-        {
-            $('#screen_id').hide();
-        }
-        else{
-            $('#screen_id').show();
-        }
-    });
-
-   function readURL(input) {
-       $('#showURL').attr('src',$(input).val());
+  $( "#screen" ).on('change',function() {
+    if($(this).val()=="app" || $(this).val()=="feedback")
+    {
+      $('#screen_id').hide();
     }
-
-
-  $(document).ready(function () {
-
-    bsCustomFileInput.init();
-    $('#createNotification').validate({
-      rules: {
-        title: {
-          required: true
-        },
-        message:{
-          required:true
-        },
-        id:{
-          required:true
-        }
-      },
-      messages: {
-        title: {
-          required: "Please enter a name"
-        },
-        message: {
-          required: "Please enter a message"
-        },
-        id:{
-          required:"Please Enter Id"
-        }
-
-      },
-      errorElement: 'span',
-      errorPlacement: function (error, element) {
-        error.addClass('invalid-feedback');
-        element.closest('.form-group').append(error);
-      },
-      highlight: function (element, errorClass, validClass) {
-        $(element).addClass('is-invalid');
-      },
-      unhighlight: function (element, errorClass, validClass) {
-        $(element).removeClass('is-invalid');
-      }
-    });
+    else{
+      $('#screen_id').show();
+    }
   });
+
+  function readURL(input) {
+   $('#showURL').attr('src',$(input).val());
+ }
+
+
+ $(document).ready(function () {
+
+  bsCustomFileInput.init();
+  $('#createNotification').validate({
+    rules: {
+      title: {
+        required: true
+      },
+      message:{
+        required:true
+      },
+      id:{
+        required:true
+      }
+    },
+    messages: {
+      title: {
+        required: "Please enter a name"
+      },
+      message: {
+        required: "Please enter a message"
+      },
+      id:{
+        required:"Please Enter Id"
+      }
+
+    },
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+      error.addClass('invalid-feedback');
+      element.closest('.form-group').append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass('is-invalid');
+    }
+  });
+});
 
 
 </script>
@@ -95,7 +95,13 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Notification</h1>
+          <h1>Send Android Notification</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/admin/home">Home</a></li>
+            <li class="breadcrumb-item">Notification</li>
+          </ol>
         </div>
       </div>
     </div><!-- /.container-fluid -->
@@ -103,66 +109,68 @@
 
   <!-- Main content -->
   <section class="content">
-    <form id="createNotification" method="post" action="/admin/notification/send">
-      @csrf
-      <div class="card card-outline card-primary">
-        <div class="card-header">
-          <h3 class="card-title">Send Android Notifications</h3>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group">
-                <label>Title Of Notification</label>
-                <input type="text" name="title" class="form-control" placeholder="Enter ...">
-              </div>
-              <div class="form-group">
-                <label>Description Of Notification</label>
-                <input type="text" name="message" class="form-control" placeholder="Enter ...">
-              </div>
-              <div class="form-group">
-                <label>Image Link Url (optional)</label>
-                <input type="text" name="image" onchange="readURL(this)" class="form-control" placeholder="Enter ...">
-              </div>
-              <div class="row">
-                <div class="col-sm-9">
-                   <div class="form-group">
-                    <label>Select Screen</label>
-                    <select name="screen" id="screen" class="form-control">
-                      <option value="app">Open App</option>
-                      <option value="project">Open Project</option>
-                      <option value="theory">Open Theory Resource</option>
-                      <option value="video">Open Video Resource</option>
-                      <option value="feedback">Open Feedback Page</option>
-                      <option value="profile" disabled="disabled">Open Profile</option>
-                      <option value="course" disabled="disabled">Open Course</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="col-sm-3" style="display: none;" id="screen_id" >
-                  <div class="form-group">
-                     <label>Id</label>
-                    <input type="number" name="id" class="form-control" placeholder="">
-                  </div>    
-                </div>
-              </div>
-              
+    <div class="row">
+      <div class="col-sm-12">
+        <form id="createNotification" method="post" action="/admin/notification/send">
+          @csrf
+          <div class="card card-outline card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Send Android Notifications</h3>
             </div>
-            <div class="col-sm-6">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label>Title Of Notification</label>
+                    <input type="text" name="title" class="form-control" placeholder="Enter ...">
+                  </div>
+                  <div class="form-group">
+                    <label>Description Of Notification</label>
+                    <input type="text" name="message" class="form-control" placeholder="Enter ...">
+                  </div>
+                  <div class="form-group">
+                    <label>Image Link Url (optional)</label>
+                    <input type="text" name="image" onchange="readURL(this)" class="form-control" placeholder="Enter ...">
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-9">
+                     <div class="form-group">
+                      <label>Select Screen</label>
+                      <select name="screen" id="screen" class="form-control">
+                        <option value="app">Open App</option>
+                        <option value="project">Open Project</option>
+                        <option value="theory">Open Theory Resource</option>
+                        <option value="video">Open Video Resource</option>
+                        <option value="feedback">Open Feedback Page</option>
+                        <option value="profile" disabled="disabled">Open Profile</option>
+                        <option value="course" disabled="disabled">Open Course</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-sm-3" style="display: none;" id="screen_id" >
+                    <div class="form-group">
+                     <label>Id</label>
+                     <input type="number" name="id" class="form-control" placeholder="">
+                   </div>    
+                 </div>
+               </div>
+             </div>
+             <div class="col-sm-6">
               <h3 style="text-align: center;">Image in Link</h3> 
               <iframe id="showURL" width="100%" height="350" src="https://cdn.dribbble.com/users/727458/screenshots/4153279/dribbble-icons.jpg">
               </iframe>
             </div>
-            </div>
-          </div>
-        </div>    
+          </div>        
+        </div>  
         <div class="card-footer">
           <button type="submit" class="btn btn-primary float-right">Send to All</button>
           <button type="submit" formaction="/admin/notification/test" class="btn btn-primary float-left">Test</button>
-        </div>
+        </div>  
       </div>
     </form>
-  </section>
+  </div>
+</div>
+</section>
 </div>
 
 @endsection
